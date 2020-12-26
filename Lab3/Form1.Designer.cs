eaeb74a000d5c318127ce971e057cb4858417cd5
RuLe0G <khlebnikov2015@yandex.ru>
2020-12-26 21:10:58 +0700

    Initial commit

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Scale = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(-4, -4);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(640, 480);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Загрузить фото";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(885, -4);
+            this.imageBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(640, 480);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(885, 484);
+            this.button16.Margin = new System.Windows.Forms.Padding(4);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(100, 28);
+            this.button16.TabIndex = 25;
+            this.button16.Text = "Сброс";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(993, 484);
+            this.button17.Margin = new System.Windows.Forms.Padding(4);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(100, 28);
+            this.button17.TabIndex = 27;
+            this.button17.Text = "SAVE";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(643, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 22);
+            this.textBox1.TabIndex = 28;
+            this.textBox1.Text = "1";
+            // 
+            // Scale
+            // 
+            this.Scale.Location = new System.Drawing.Point(802, 47);
+            this.Scale.Name = "Scale";
+            this.Scale.Size = new System.Drawing.Size(75, 23);
+            this.Scale.TabIndex = 29;
+            this.Scale.Text = "Scale";
+            this.Scale.UseVisualStyleBackColor = true;
+            this.Scale.Click += new System.EventHandler(this.Scale_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(737, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(59, 22);
+            this.textBox2.TabIndex = 28;
+            this.textBox2.Text = "1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(644, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 23);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(750, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 23);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "Поворот";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(644, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 32;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(644, 207);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 34;
+            this.button5.Text = "shearing";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(726, 207);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(45, 22);
+            this.textBox4.TabIndex = 35;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(786, 207);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(45, 22);
+            this.textBox5.TabIndex = 35;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(644, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 36;
+            this.button4.Text = "reflection";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(725, 247);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(45, 22);
+            this.textBox6.TabIndex = 35;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(786, 247);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(45, 22);
+            this.textBox7.TabIndex = 35;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1541, 652);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Scale);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Scale;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
---
+++ Lab3/Form1.cs
@@ -0,0 +1,130 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+
+        private UCDLRAOCI myclass = new UCDLRAOCI();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //открыть картинку
+
+        private string OpenFile()
+        {
+            string fileName = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = ("Файлы изображений | *.jpg; *.jpeg; *.jpe; *.jfif; *.png");
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла            
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                fileName = openFileDialog.FileName;
+            }
+            return fileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myclass.Source(OpenFile());
+            imageBox1.Image = myclass.sourceImage;
+            imageBox2.Image = myclass.sourceImage;
+        }
+        private void button16_Click(object sender, EventArgs e)
+        {
+            myclass.MainImage();
+            imageBox2.Image = myclass.MainImageExp;
+        }
+
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = ("Файлы изображений | *.jpg; *.jpeg; *.jpe; *.jfif; *.png");
+            var result = saveFileDialog.ShowDialog(); // открытие диалога выбора файла            
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = saveFileDialog.FileName;
+                myclass.saveJpeg(fileName);
+            }
+        }
+
+        Point[] pts = new Point[4];
+        private void Scale_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.Scale(double.Parse(textBox1.Text, CultureInfo.InvariantCulture), double.Parse(textBox2.Text, CultureInfo.InvariantCulture));
+        }
+        
+        
+        int c = 0;
+        private void imageBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            var ingCopy = myclass.sourceImage.Copy();
+            int x = (int)(e.Location.X / imageBox1.ZoomScale);
+            int y = (int)(e.Location.Y / imageBox1.ZoomScale);
+
+            pts[c] = new Point(x, y);
+            c++;
+            if (c>=4)
+            {
+                c = 0;
+            }
+
+            int radius = 2;
+            int thickness = 2; 
+            var color = new Bgr(Color.Blue).MCvScalar;
+            // функция, рисующая на изображении круг с заданными параметрами
+            foreach (Point po in pts)
+            {
+                CvInvoke.Circle(ingCopy, po, radius, color, thickness);
+            }
+            
+            imageBox1.Image = ingCopy;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            imageBox2.Image = myclass.homography(pts);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.rotate(double.Parse(textBox3.Text),pts[0]);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.shearing(10, 10);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox4.Text);
+            double y = double.Parse(textBox5.Text);
+
+            imageBox2.Image = myclass.shearing(x, y);
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            int X = int.Parse(textBox6.Text);
+            int Y = int.Parse(textBox7.Text);
+            imageBox2.Image = myclass.reflection(X, Y);
+        }
+    }
+}
